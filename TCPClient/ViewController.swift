893fa493a30df650d6c71fc6ce51571662074a0a
Riccardo Rizzo <riccardo.rizzo@machinemax.com>
2023-07-27 15:57:32 +0100

    Initial Commit

---
+++ TCPClient/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TCPClient
+//
+//  Created by Riccardo Rizzo on 27/07/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
