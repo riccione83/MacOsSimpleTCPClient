50fd6650affe5fe20794091303faf18dc50fe3d1
Riccardo Rizzo <riccardo.rizzo@machinemax.com>
2023-07-28 09:09:03 +0100

    first commit

---
+++ TCPClient/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  TCPClient
+//
+//  Created by Riccardo Rizzo on 28/07/2023.
+//
+
+import Foundation
---
+++ TCPClient/TCPClass.swift
@@ -0,0 +1,8 @@
+//
+//  TCPClass.swift
+//  TCPClient
+//
+//  Created by Riccardo Rizzo on 27/07/2023.
+//
+
+import Foundation
